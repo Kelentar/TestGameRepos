02675795163690ed896b5e5c69d9b315e42f77e0
Kelentar <61499503+Kelentar@users.noreply.github.com>
2020-04-29 18:42:15 +0300

    bup

---
+++ Assets/Script/BackgroundScripts/Paralax.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paralax : MonoBehaviour
+{
+    public float Lenght, startpos;
+    public GameObject cam;
+    public float parallaxEffect;
+
+    void Start()
+    {
+        startpos = transform.position.x;
+        Lenght = GetComponent<SpriteRenderer>().bounds,size.x;
+    }
+
+    void Update()
+    {
+        float
+    }
+}
